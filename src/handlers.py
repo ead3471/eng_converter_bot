4fb1d6593310eee1d39852fac8eb3bc2834b3795
ead3471 <ead3471@gmail.com>
2022-12-16 16:50:26 +0300

    Full Pressure panel added

--- src/handlers.py
+++ src/handlers.py
@@ -1,36 +1,41 @@
 from misc import dp, bot
 
 from aiogram import types
-from keyboards import main_keyboard, main_keyboard_buttons, temperature_units_keyboard, temperature_eu_buttons
+from keyboards import (main_keyboard,
+                       main_keyboard_buttons,
+                       create_eu_keyboard_for_measure,
+                       get_pressed_eu_button)
 import keyboards
-from eng_unit_converter.measure import Measure
 from aiogram.dispatcher import FSMContext
 from state_machine import Convert
-
-
-current_measure_class = None
-current_measure_eu = None
-current_measure_value = 0
-current_measure: Measure = None
+import aiogram.utils.markdown as md
 
 
 @dp.message_handler(commands=['start'])
 async def start_converting(message: types.Message, state: FSMContext):
-
     await Convert.choosing_measure_type.set()
     await bot.send_message(chat_id=message.from_user.id,
                            text="Выберете тип измерения",
                            reply_markup=main_keyboard)
 
 
-@dp.callback_query_handler(lambda c: c.data and c.data.endswith('measure'), state=Convert.choosing_measure_type)
-async def process_measure_type(callback_query: types.CallbackQuery, state: FSMContext):
+@dp.callback_query_handler(lambda c: c.data and c.data == 'cancel',
+                           state='*')
+async def process_cancel(callback_query: types.CallbackQuery,
+                         state: FSMContext):
+    await Convert.choosing_measure_type.set()
+    await bot.send_message(chat_id=callback_query.from_user.id,
+                           text="Выберете тип измерения",
+                           reply_markup=main_keyboard)
+
+
+@dp.callback_query_handler(lambda c: c.data and c.data.endswith('measure'),
+                           state=Convert.choosing_measure_type)
+async def process_measure_type_choosen(callback_query: types.CallbackQuery,
+                                       state: FSMContext):
     pressed_button = keyboards.get_pressed_button(main_keyboard_buttons,
                                                   callback_query.data)
-    selected_measure = pressed_button.measure_class
-
-    global current_measure_class
-    current_measure_class = selected_measure
+    await state.update_data(measure_class=pressed_button.measure_class)
 
     await bot.send_message(callback_query.from_user.id,
                            f'Выбрана {pressed_button.title}. Укажите значение')
@@ -39,42 +44,55 @@ async def process_measure_type(callback_query: types.CallbackQuery, state: FSMCo
 
 @dp.message_handler(state=Convert.set_value)
 async def process_value(message: types.Message, state: FSMContext):
-    global current_measure_value
     current_measure_value = float(message.text)
-    await bot.send_message(message.chat.id, "Укажите единицы измерения", reply_markup=temperature_units_keyboard)
+    measure_class = (await state.get_data())['measure_class']
+    await state.update_data(value=float(current_measure_value))
+    await bot.send_message(message.chat.id, "Укажите единицы измерения",
+                           reply_markup=create_eu_keyboard_for_measure(measure_class))
     await Convert.next()
 
 
-@dp.callback_query_handler(lambda c: c.data and c.data.endswith('_temp_units'), state=Convert.choosing_eng_unit)
-async def process_eng_units(callback_query: types.CallbackQuery, state: FSMContext):
-    pressed_button = keyboards.get_pressed_button(temperature_eu_buttons,
-                                                  callback_query.data)
+@dp.callback_query_handler(lambda c: c.data and c.data.endswith('_units'),
+                           state=Convert.choosing_eng_unit)
+async def process_eng_units_choosen(callback_query: types.CallbackQuery,
+                                    state: FSMContext):
+    user_data = await state.get_data()
+    measure_class = user_data['measure_class']
+    pressed_button = keyboards.get_pressed_eu_button(measure_class,
+                                                     callback_query.data)
     selected_units = pressed_button.measure_class
+    await state.update_data(eu=selected_units)
 
-    global current_measure_eu, current_measure, current_measure_class
-    current_measure_eu = selected_units
+    current_measure = user_data['measure_class'](
+        user_data['value'], selected_units)
 
-    current_measure = current_measure_class(
-        current_measure_value, current_measure_eu)
+    await state.update_data(measure=current_measure)
 
     await bot.send_message(callback_query.from_user.id,
-                           f'Текущее значение {current_measure}. Укажите единицы для перевода',
-                           reply_markup=temperature_units_keyboard
+                           (f'Текущее значение {current_measure}.\n'
+                            'Укажите единицы для перевода'),
+                           reply_markup=create_eu_keyboard_for_measure(
+                               measure_class)
                            )
     await Convert.next()
 
 
-@dp.callback_query_handler(lambda c: c.data and c.data.endswith('_temp_units'), state=Convert.choosing_new_eng_unit)
-async def process_new_eng_units(callback_query: types.CallbackQuery, state: FSMContext):
-    pressed_button = keyboards.get_pressed_button(temperature_eu_buttons,
-                                                  callback_query.data)
+@ dp.callback_query_handler(lambda c: c.data and c.data.endswith('_units'),
+                            state=Convert.choosing_new_eng_unit)
+async def process_convertion_units_choosen(callback_query: types.CallbackQuery,
+                                           state: FSMContext):
 
-    global current_measure_eu, current_measure, current_measure_class
+    user_data = await state.get_data()
+    measure_class = user_data['measure_class']
+    pressed_button = keyboards.get_pressed_eu_button(measure_class,
+                                                     callback_query.data)
+
     new_measure_eu = pressed_button.measure_class
 
-    converted_measure = current_measure.convert_to(new_measure_eu)
+    converted_measure = user_data['measure'].convert_to(new_measure_eu)
 
     await bot.send_message(callback_query.from_user.id,
-                           f'Конвертированное значение {converted_measure}.',
+                           (f'Конвертированное значение\n{converted_measure}\n'
+                            'Укажите новые единицы для перевода или вернитесь'
+                            ' в начало'),
                            )
-    await Convert.next()
--- src/keyboards.py
+++ src/keyboards.py
@@ -1,17 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from eng_unit_converter.measure import Measure, Temperature, MassFlow, ThermoResistor, AnalogSensorMeasure, Pressure
 from dataclasses import dataclass
-from typing import List
-
-
-def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
-    """
-    Создаёт реплай-клавиатуру с кнопками в один ряд
-    :param items: список текстов для кнопок
-    :return: объект реплай-клавиатуры
-    """
-    row = [KeyboardButton(text=item) for item in items]
-    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+from typing import List, Dict
 
 
 @dataclass
@@ -36,7 +26,8 @@ main_keyboard_buttons: List[List[MeasureButton]] = [
 ]
 
 
-def create_keyboard(buttons_list: List[List[MeasureButton]], row_width: int = 3) -> InlineKeyboardMarkup:
+def create_keyboard(buttons_list: List[List[MeasureButton]],
+                    row_width: int = 3) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(row_width=row_width)
     for row in buttons_list:
         buttons_list = [
@@ -44,27 +35,66 @@ def create_keyboard(buttons_list: List[List[MeasureButton]], row_width: int = 3)
             for button in row
         ]
         keyboard.add(*buttons_list)
+    keyboard.add(InlineKeyboardButton("В начало", callback_data="cancel"),
+                 # InlineKeyboardButton("Шаг назад", callback_data="step_back")
+                 )
     return keyboard
+
+
+def create_eu_keyboard_for_measure(measure_type: type) -> InlineKeyboardMarkup:
+    return create_keyboard(measure_units[measure_type])
+
+
+def get_pressed_eu_button(measure_type: type,
+                          callback_data: str) -> MeasureButton:
+    return get_pressed_button(measure_units[measure_type], callback_data)
 
 
 main_keyboard = create_keyboard(main_keyboard_buttons)
 
 
-def get_pressed_button(keyboard_buttons: List[List[MeasureButton]], callback_data: str) -> MeasureButton:
+def get_pressed_button(keyboard_buttons: List[List[MeasureButton]],
+                       callback_data: str) -> MeasureButton:
     for row in keyboard_buttons:
         for button in row:
             if button.callback == callback_data:
                 return button
-
     return Temperature
 
 
-temperature_eu_buttons: List[List[MeasureButton]] = [
-    [
+measure_units: Dict[type, List[List[MeasureButton]]] = {
+    Temperature: [[
         MeasureButton('C', 'celsius_temp_units', Temperature.SupportedUnits.C),
         MeasureButton('F', 'fahrenheit_temp_units',
                       Temperature.SupportedUnits.F),
         MeasureButton('K', 'kelvin_temp_units', Temperature.SupportedUnits.K),
+    ]],
+    Pressure: [
+        [
+            MeasureButton('атм', 'atm_press_units',
+                          Pressure.SupportedUnits.atm),
+            MeasureButton('Па', 'Pa_press_units',
+                          Pressure.SupportedUnits.Pa),
+            MeasureButton('кПа', 'kPa_press_units',
+                          Pressure.SupportedUnits.kPa),
+            MeasureButton('МПа', 'MPa_press_units',
+                          Pressure.SupportedUnits.MPa),
+            MeasureButton('кгс/см2', 'kgs_sm2_press_units',
+                          Pressure.SupportedUnits.kgs_sm_2),
+            MeasureButton('кгс/м2', 'kgs_m2_press_units',
+                          Pressure.SupportedUnits.kgs_m_2),
+
+        ],
+        [
+            MeasureButton('мм.вод.столба', 'mmh20_press_units',
+                          Pressure.SupportedUnits.mm_h20),
+            MeasureButton('м.вод.столба', 'mh20_press_units',
+                          Pressure.SupportedUnits.m_h20),
+            MeasureButton('мм.рт.ст.', 'bar_press_units',
+                          Pressure.SupportedUnits.mm_hg),
+            MeasureButton('бар', 'bar_press_units',
+                          Pressure.SupportedUnits.bar),
+        ]
+
     ]
-]
-temperature_units_keyboard = create_keyboard(temperature_eu_buttons)
+}
